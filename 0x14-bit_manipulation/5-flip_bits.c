2efb219cd2f5c23ebb82395c87bb1e1307dabc9a
MmakgaboMapotse <mmakgabomapotse@gmail.com>
2023-08-31 14:51:20 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-	* flip_bits - counts the number of bits needed to flip to transform n to m
+	* flip_bits - calculates the Hamming distance between two numbers
 	* @n: first number
 	* @m: second number
 	*
-	* Return: number of bits to flip
+	* Return: number of differing bits
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor_result = n ^ m;	
+	unsigned long int difference = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_result > 0)
+	while (difference > 0)
 	{
-	count += xor_result & 1;
-	xor_result >>= 1;
+	count += difference & 1;
+	difference >>= 1;
 	}
 
-	return count;
+	return (count);
 }
